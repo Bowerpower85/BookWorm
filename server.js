872feea7bcbc6d282975595bcd3bdb00911f74ea
Jonathan Bower <bowerpower85@gmail.com>
2020-12-08 19:48:17 -0800

    Set up app.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 const logger = require("morgan");
 
 // Requirer models
@@ -18,6 +19,10 @@ app.use(express.json);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/savedBooks";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Send every request to the React app
 // Define any API routes before this runs
