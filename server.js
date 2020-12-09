7e901857c34e165184b21b65a9e2d532fd66ff61
Jonathan Bower <bowerpower85@gmail.com>
2020-12-08 17:43:30 -0800

    Added MongoDb mongoose to connect to server and morgan to handle requests.

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mongoose = require("mongoose");
+const logger = require("morgan");
 
 // Requirer models
 const db = require("./models")
+
+// Morgan for logging requests
+app.use(logger("dev"));
+
+// Define middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
